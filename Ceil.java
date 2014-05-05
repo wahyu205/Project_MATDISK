514ae3e1bbc805fa3e56f2db141345f060a4ea4f
Wahyu Kartika Hadi <wahzuzumy@gmail.com>
2014-05-05 16:39:10 +0700

    Create Ceil.java

---
+++ Ceil.java
@@ -0,0 +1,15 @@
+public class Ceil {
+
+    public static void main(String[] args) {
+
+        double E1 = Math.ceil(15.156);
+
+        double E2 = Math.ceil(-15.156);
+
+        double E3 = Math.ceil(15);
+
+        System.out.println("Math ceil value :"+E1);
+        System.out.println("Math ceil value :"+E2);
+        System.out.println("Math ceil value :"+E3);
+    }
+}
