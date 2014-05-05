ce4a4fa3595c91e10d36ffabad36c0a01c58801f
Wahyu Kartika Hadi <wahzuzumy@gmail.com>
2014-05-05 16:40:54 +0700

    Create Floor.java

---
+++ Floor.java
@@ -0,0 +1,18 @@
+public class Floor {
+
+    public static void main(String[] args) {
+
+        double E1 = Math.floor(14.2345);
+
+        double E2 = Math.floor(14.5345);
+
+        double E3 = Math.floor(-14.2345);
+
+        double E4 = Math.floor(-14.5345);
+
+        System.out.println("Math floor value :"+E1);
+        System.out.println("Math floor value :"+E2);
+        System.out.println("Math floor value :"+E3);
+        System.out.println("Math floor value :"+E4);
+    }
+}
