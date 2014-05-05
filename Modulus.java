1e264935b9834bc4a7b87c5666253fce3b7b0d36
Wahyu Kartika Hadi <wahzuzumy@gmail.com>
2014-05-05 16:41:35 +0700

    Create Modulus.java

---
+++ Modulus.java
@@ -0,0 +1,15 @@
+class Modululus {
+
+  public static void main (String args[]) {
+
+    int i = 10;
+    int j = 3;
+
+    System.out.println("i is " + i);
+    System.out.println("j is " + j);
+  
+    int k = i % j;
+    System.out.println("i%j is " + k);
+  }
+
+}
